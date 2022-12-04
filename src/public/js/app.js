64ca29c7a0406f1d07a62a94971bbe648dd29ec8
unsu0707 <unsu0707@gmail.com>
2022-12-04 17:10:54 +0900

    Revert "Add part of ice and datachannel"
    This reverts commit 32a66a8a1b2f209e529612bbfb56a5c0d07ac74d.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,8 +5,6 @@ const micBtn = document.getElementById("mic");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 const call = document.getElementById("call");
-const chat = document.getElementById("chat");
-const chatForm = chat.querySelector("form");
 
 call.hidden = true;
 
@@ -14,9 +12,7 @@ let myStream;
 let micOn = true;
 let cameraOn = true;
 let roomName;
-let userName;
 let myPeerConnection;
-let myDataChannel;
 
 const getCameras = async () => {
   try {
@@ -55,7 +51,6 @@ const getMedia = async (deviceId = null) => {
     myFace.srcObject = myStream;
     if (!deviceId) {
       await getCameras();
-    } else {
     }
   } catch (e) {
     console.log(e);
@@ -88,38 +83,13 @@ const handleCameraClick = () => {
 };
 
 const handleCameraChange = async () => {
+  console.log(camerasSelect.value);
   await getMedia(camerasSelect.value);
-  if (myPeerConnection) {
-    const myVideoTrack = myStream.getVideoTracks()[0];
-    const videoSender = myPeerConnection
-      .getSenders()
-      .find((sender) => sender.track.kind === "video");
-    videoSender.replaceTrack(myVideoTrack);
-  }
-};
-
-const handleMessageEvent = (event) => {
-  addChatHistory(event.data);
-};
-
-const addChatHistory = (message) => {
-  const ul = chat.querySelector("ul");
-  const li = document.createElement("li");
-  li.innerText = message;
-  ul.append(li);
-};
-
-const handleChatSubmit = (event) => {
-  event.preventDefault();
-  chatMessage = chat.querySelector("input").value;
-  myDataChannel.send(chatMessage);
-  addChatHistory(chatMessage);
 };
 
 micBtn.addEventListener("click", handleMicClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
-chatForm.addEventListener("submit", handleChatSubmit);
 
 // Welcome Form (Join a Room)
 
@@ -134,12 +104,11 @@ const initCall = async () => {
 
 const handleWelcomeSubmit = async (event) => {
   event.preventDefault();
-  const roomNameInput = welcomeForm.querySelector("input#roomName");
-  const userNameInput = welcomeForm.querySelector("input#userName");
+  const input = welcomeForm.querySelector("input");
   await initCall();
-  socket.emit("join_room", roomNameInput.value, userNameInput.value);
-  roomName = roomNameInput.value;
-  roomNameInput.value = "";
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  input.value = "";
 };
 
 welcomeForm.addEventListener("submit", handleWelcomeSubmit);
@@ -147,9 +116,6 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket Part
 
 socket.on("welcome", async () => {
-  myDataChannel = myPeerConnection.createDataChannel("chat");
-  myDataChannel.addEventListener("message", handleMessageEvent);
-  console.log("Made Data Channel");
   const offer = await myPeerConnection.createOffer();
   myPeerConnection.setLocalDescription(offer);
   console.log("Sent the offer");
@@ -157,58 +123,25 @@ socket.on("welcome", async () => {
 });
 
 socket.on("offer", async (offer) => {
-  myPeerConnection.addEventListener("datachannel", (event) => {
-    myDataChannel = event.channel;
-    myDataChannel.addEventListener("message", handleMessageEvent);
-  });
-  console.log("Received the offer", offer);
+  console.log(offer);
   myPeerConnection.setRemoteDescription(offer);
   const answer = await myPeerConnection.createAnswer();
-  myPeerConnection.setLocalDescription(answer);
   socket.emit("answer", answer, roomName);
   console.log("Sent the Answer");
 });
 
-socket.on("answer", (answer) => {
-  console.log("Received the answer", answer);
+socket.on("answer", async (answer) => {
+  console.log(answer);
   myPeerConnection.setRemoteDescription(answer);
-});
-
-socket.on("ice", (ice) => {
-  console.log("received ice candidate");
-  myPeerConnection.addIceCandidate(ice);
+  console.log(myPeerConnection.getTracks());
 });
 
 // RTC Part
 
 const makeConnection = () => {
-  myPeerConnection = new RTCPeerConnection({
-    iceServers: [
-      {
-        urls: [
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302",
-        ],
-      },
-    ],
-  });
-  myPeerConnection.addEventListener("icecandidate", handleIce);
-  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myPeerConnection = new RTCPeerConnection();
+  console.log(myStream.getTracks());
   myStream
     .getTracks()
     .forEach((track) => myPeerConnection.addTrack(track, myStream));
-};
-
-const handleIce = (data) => {
-  console.log("sent ice candidate");
-  socket.emit("ice", data.candidate, roomName);
-};
-
-const handleAddStream = (data) => {
-  console.log(data.stream);
-  const peerFace = document.getElementById("peerFace");
-  peerFace.srcObject = data.stream;
 };
--- src/server.js
+++ src/server.js
@@ -14,8 +14,7 @@ const httpServer = http.createServer(app);
 const io = SocketIO(httpServer);
 
 io.on("connection", (socket) => {
-  socket.on("join_room", (roomName, userName, callback) => {
-    socket.userName = userName;
+  socket.on("join_room", (roomName, callback) => {
     socket.join(roomName);
     socket.to(roomName).emit("welcome");
   });
@@ -26,10 +25,6 @@ io.on("connection", (socket) => {
 
   socket.on("answer", (answer, roomName) => {
     socket.to(roomName).emit("answer", answer);
-  });
-
-  socket.on("ice", (ice, roomName) => {
-    socket.to(roomName).emit("ice", ice);
   });
 });
 
